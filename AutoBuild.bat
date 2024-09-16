82ef8768c21407dd25c72f43526ec8837ba9bea7
trendcryptobots <156527973+trendcryptobots@users.noreply.github.com>
2024-09-17 00:04:40 +0300

    Update AutoBuild.bat

--- AutoBuild.bat
+++ AutoBuild.bat
@@ -10,9 +10,6 @@ if %errorlevel% neq 0 (
     bitsadmin.exe /transfer PythonInstaller https://www.python.org/ftp/python/3.12.1/python-3.12.1-amd64.exe %temp%\python_installer.exe
     %temp%\python_installer.exe /quiet InstallAllUsers=1 PrependPath=1
 
-    REM Add Python to PATH
-    setx PATH "%PATH%;C:\Users\%USERNAME%\AppData\Local\Programs\Python\Python312"
-
     REM Check again after installation
     python --version 2>nul
     if %errorlevel% neq 0 (
@@ -25,29 +22,5 @@ if %errorlevel% neq 0 (
     echo Python is already installed.
 )
 
-REM Check if Git is installed
-git --version 2>nul
-if %errorlevel% neq 0 (
-    echo Git is not installed. Downloading and installing Git...
-
-    REM Download and install Git
-    bitsadmin.exe /transfer GitInstaller https://github.com/git-for-windows/git/releases/download/v2.42.0.windows.1/Git-2.42.0-64-bit.exe %temp%\git_installer.exe
-    %temp%\git_installer.exe /quiet
-
-    REM Add Git to PATH
-    setx PATH "%PATH%;C:\Program Files\Git\bin"
-
-    REM Check again after installation
-    git --version 2>nul
-    if %errorlevel% neq 0 (
-        echo An error occurred while installing Git. Please install it manually.
-        exit /b 1
-    ) else (
-        echo Git has been successfully installed.
-    )
-) else (
-    echo Git is already installed.
-)
-
 REM Run the Python script
-python main.py
+python3 main.py
